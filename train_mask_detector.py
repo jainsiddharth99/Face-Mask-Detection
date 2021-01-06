12a81e9ce521e6071edb3d52fc3cb0b36ac1f521
Sid <44680199+jainsiddharth99@users.noreply.github.com>
2021-01-06 11:58:05 +0530

    Update train_mask_detector.py

--- train_mask_detector.py
+++ train_mask_detector.py
@@ -81,13 +81,11 @@ model = Model(inputs=baseModel.input, outputs=headModel)
 for layer in baseModel.layers:
 	layer.trainable = False
 
-# compile our model
 print("[INFO] compiling model...")
 opt = Adam(lr=INIT_LR, decay=INIT_LR / EPOCHS)
 model.compile(loss="binary_crossentropy", optimizer=opt,
 	metrics=["accuracy"])
 
-# train the head of the network
 print("[INFO] training head...")
 H = model.fit(
 	aug.flow(trainX, trainY, batch_size=BS),
@@ -96,23 +94,17 @@ H = model.fit(
 	validation_steps=len(testX) // BS,
 	epochs=EPOCHS)
 
-# make predictions on the testing set
 print("[INFO] evaluating network...")
 predIdxs = model.predict(testX, batch_size=BS)
 
-# for each image in the testing set we need to find the index of the
-# label with corresponding largest predicted probability
 predIdxs = np.argmax(predIdxs, axis=1)
 
-# show a nicely formatted classification report
 print(classification_report(testY.argmax(axis=1), predIdxs,
 	target_names=lb.classes_))
-
-# serialize the model to disk
 print("[INFO] saving mask detector model...")
 model.save("mask_detector.h5")
 
-# plot the training loss and accuracy
+
 N = EPOCHS
 plt.style.use("ggplot")
 plt.figure()
